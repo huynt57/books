bbece6e5a4994684fd2e602b1fcf9dfc8e47e3c2
huynt <huynt57@gmail.com>
2015-09-15 13:52:19 +0700

    fix GD

--- protected/views/book/index.php
+++ protected/views/book/index.php
@@ -7,30 +7,32 @@
                 </h3>
             </div>
             <div class="box-content nopadding">
-                <table class="table table-hover table-nomargin table-bordered usertable">
+                <table class="table table-hover table-nomargin table-bordered dataTable">
                     <thead>
                         <tr class='thefilter'>
 <!--                            <th class='with-checkbox'></th>-->
                             <th>ID</th>
                             <th>Name</th>
-                            <th class='hidden-350'>Author</th>
-                            <th class='hidden-1024'>Publisher</th>
-                            <th class='hidden-480'>Year</th>
-                            <th class='hidden-480'>Description</th>
-                            <th class='hidden-480'>Status</th>
-                            <th class='hidden-480'>Action</th>
+                            <th>Image</th>
+                            <th>Author</th>
+                            <th>Publisher</th>
+                            <th>Year</th>
+                            <th>Description</th>
+                            <th>Status</th>
+                            <th>Action</th>
                         </tr>
-                        <tr>
-<!--                            <th class='with-checkbox'><input type="checkbox" name="check_all" id="check_all"></th>-->
+<!--                        <tr>
+                            <th class='with-checkbox'><input type="checkbox" name="check_all" id="check_all"></th>
                             <th>ID</th>
                             <th>Name</th>
-                            <th class='hidden-350'>Author</th>
-                            <th class='hidden-1024'>Publisher</th>
-                            <th class='hidden-480'>Year</th>
-                            <th class='hidden-480'>Description</th>
-                            <th class='hidden-480'>Status</th>
-                            <th class='hidden-480'>Action</th>
-                        </tr>
+                            <th>Image</th>
+                            <th>Author</th>
+                            <th>Publisher</th>
+                            <th>Year</th>
+                            <th>Description</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>-->
                     </thead>
                     <tbody>
                         <?php foreach ($books as $book): ?>
@@ -40,19 +42,20 @@
                                 </td>-->
                                 <td><?php echo $book->id ?></td>
                                 <td><?php echo $book->book_name ?></td>
+                                <td><img src="<?php echo $book->book_image ?>" height="100" width="100" /></td>
                                 <td><?php echo $book->book_author ?></td>
                                 <td><?php echo $book->book_publisher ?></td>
                                 <td><?php echo $book->book_year ?></td>
                                 <td><?php echo $book->book_description ?></td>
                                 <td class='hidden-350'>
-                                    <?php if($book->status == 1):?>
-                                    <span class="label label-satgreen"> Active
-                                    </span>
-                                    <?php endif;?>
-                                    <?php if($book->status != 1):?>
-                                    <span class="label label-red"> Inactive
-                                    </span>
-                                    <?php endif;?>
+                                    <?php if ($book->status == 1): ?>
+                                        <span class="label label-satgreen"> Active
+                                        </span>
+                                    <?php endif; ?>
+                                    <?php if ($book->status != 1): ?>
+                                        <span class="label label-red"> Inactive
+                                        </span>
+                                    <?php endif; ?>
                                 </td>
                                 <td class='hidden-480'>
                                     <a href="#" class="btn" rel="tooltip" title="View"><i class="icon-search"></i></a>
